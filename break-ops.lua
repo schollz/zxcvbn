4b008043bf956dd477c14a583453c8ee08c9023e
Zack <zack.scholl@gmail.com>
2022-09-15 10:17:28 -0700

    first commit

---
+++ break-ops.lua
@@ -0,0 +1,57 @@
+-- break ops
+--
+--
+-- llllllll.co/t/lightsout
+--
+--
+--
+--    ▼ instructions below ▼
+
+
+grid__=include("lib/ggrid")
+MusicUtil = require "musicutil"
+lattice=require("lattice")
+
+engine.name="PolyPerc"
+
+function init()
+  grid_=grid__:new()
+
+  -- start lattice
+  local sequencer=lattice:new{
+    ppqn=96
+  }
+  divisions={1/2,1/4,1/8,1/16,1/2,1/4,1/8,1/16,1/2,1/4,1/8,1/16,1/2,1/4,1/8,1/16}
+  for i=1,16 do 
+    local step=0
+    sequencer:new_pattern({
+      action=function(t)
+        step=step+1
+      end,
+      division=divisions[i],
+    })
+  end
+  sequencer:hard_restart()
+
+end
+
+
+function enc(k,d)
+
+end
+
+function key(k,z)
+
+end
+
+function redraw()
+  screen.clear()
+  screen.move(32,64)
+  screen.text("break ops")
+
+  screen.update()
+end
+
+function rerun()
+  norns.script.load(norns.state.script)
+end
