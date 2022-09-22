6c9a785094a36c711ae130e1c9287fe01c9443bd
Zack Scholl <zack.scholl@gmail.com>
2022-09-22 04:33:12 -0700

    track works

--- lib/track.lua
+++ lib/track.lua
@@ -9,19 +9,73 @@ function Track:new(o)
 end
 
 function Track:init()
+  -- initialize parameters
+  params:add_option(self.id.."track_type","type",{"sliced sample","melodic sample","midi","crow","engine"})
+  params:set_action(self.id.."track_type",function(x)
+    -- rerun show/hiding
+    self:select(self.selected)
+  end)
+  -- sliced sample
   params:add_file(self.id.."sample_file","file",_path.audio.."break-ops")
   params:set_action(self.id.."sample_file",function(x)
+    print("sample_file",x)
     if util.file_exists(x) and string.sub(x,-1)~="/" then
       self:load_sample(x)
     end
   end)
-  params:add{type="binary",name="play",id=self.id.."sample_play",behavior="toggle",action=function(v)
+  params:add_number(self.id.."sample_bpm","source bpm",10,200,math.floor(clock.get_tempo()))
+  params:add{type="binary",name="play",id=self.id.."track_play",behavior="toggle",action=function(v)
   end}
-  params:add_option(self.id.."sample_division","division",possible_division_options,5)
+  params:add_option(self.id.."track_division","division",possible_division_options,5)
 
-  self.params={}
-  self.params.share={"",""}
-  self.params.midi={"",""} -- only show if midi is enabled
+  self.params={shared={"track_type","track_play","track_division"}}
+  self.params["sliced sample"]={"sample_file","sample_bpm"} -- only show if midi is enabled
+
+  -- initialize track data
+  self.state="vterm"
+  self.vterm=vterm_:new()
+  self.sample=sample_:new()
+end
+
+function Track:select(selected)
+  self.selected=selected
+  for k,ps in pairs(self.params) do
+    for _,p in ipairs(ps) do
+      if selected and (k=="shared" or k==params:string(self.id.."track_type")) then
+        params:show(self.id..p)
+      else
+        params:hide(self.id..p)
+      end
+    end
+  end
+  debounce_fn["menu"]={
+    1,function()
+      _menu.rebuild_params()
+    end
+  }
+end
+
+function Track:load_sample(path)
+  self.sample:load_sample(path)
+  self.state="sample"
+end
+
+-- base functions
+
+function Track:keyboard(k,v)
+  self[self.state]:keyboard(k,v)
+end
+
+function Track:enc(k,d)
+  self[self.state]:enc(k,d)
+end
+
+function Track:key(k,z)
+  self[self.state]:key(k,z)
+end
+
+function Track:redraw()
+  self[self.state]:redraw()
 end
 
 return Track
