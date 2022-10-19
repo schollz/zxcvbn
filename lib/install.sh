fb3d85c8b246492ad44334d5e1b52369119ef3f0
Zack <zack.scholl@gmail.com>
2022-10-19 13:46:45 -0700

    better

--- lib/install.sh
+++ lib/install.sh
@@ -10,5 +10,4 @@ cd aubio && sudo ./waf install --destdir=/
 sudo ldconfig
 cd .. && rm -rf aubio
 # apply patch automatically
-cp /home/we/dust/code/zxcvbn/lib/keyboard.patch /home/we/dust/norns/
-cd /home/we/dust/norns && git apply keyboard.patch
+cd /home/we/dust/norns && git reset --hard HEAD && git apply /home/we/dust/code/zxcvbn/lib/keyboard.patch
