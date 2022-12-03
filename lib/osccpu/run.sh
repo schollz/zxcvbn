a99626dbdc45e21c6285ba0dc0c46f1f1aad039d
Zack Scholl <zack.scholl@gmail.com>
2022-12-02 20:44:24 -0800

    longer time

--- lib/osccpu/run.sh
+++ lib/osccpu/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ps -ef | grep 'osccpu' | grep -v grep | grep -v run | awk '{print $2}' | xargs -r kill -9
 cd "$(dirname "$0")"
-nohup ./osccpu -n scsynth -d 3 >/dev/null 2>&1 &
+nohup ./osccpu -n scsynth -d 5 >/dev/null 2>&1 &
